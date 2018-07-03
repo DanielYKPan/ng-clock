05a966b41515a8af07ba12aa4e7f9a1da3a94c3d
Yaokun Pan <myron.yk.pan@gmail.com>
2018-07-03 16:31:04 +1000

    Added clock component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app';
+
+    get clock$(): Observable<Date> {
+        return this.appService.clock;
+    }
+
+    constructor( private appService: AppService ) {
+    }
+
+    public ngOnInit(): void {
+        this.appService.runClock();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule, MatToolbarModule } from '@angular/material';
+import { ClockComponent } from './clock/clock.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        ClockComponent
     ],
     imports: [
         BrowserModule,
